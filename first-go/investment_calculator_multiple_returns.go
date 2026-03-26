39bec30f19ecef587f11a8adc2c904676cdf0a47
royalbytes <45887761+discovered1086@users.noreply.github.com>
2026-03-25 21:23:52 -0500

    different return syntax added

--- first-go/investment_calculator_multiple_returns.go
+++ first-go/investment_calculator_multiple_returns.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"strconv"
 )
 
 const inflationRate float64 = 2.5
@@ -17,15 +18,27 @@ func main() {
 	fmt.Print("Enter expected return: ")
 	fmt.Scan(&expectedReturnRate)
 
-	futureValue, futureActualReturnValue := calculateValue(investment, years, expectedReturnRate)
+	futureValue, futureActualReturnValue := calculateValueDifferentReturn(investment, years, expectedReturnRate)
 
-	fmt.Println(formatNumber(futureValue, 2, "Future value: ", "f"))
-	fmt.Println(formatNumber(futureActualReturnValue, 2,
+	fmt.Println(printNumber(futureValue, 2, "Future value: ", "f"))
+	fmt.Println(printNumber(futureActualReturnValue, 2,
 		"Future adjustment float value: ", "f"))
 }
 
-func calculateValue(investment, expectedReturnRate, years float64) (float64, float64) {
-	futureValue := investment * math.Pow(1+expectedReturnRate/100, years)
-	futureActualReturnValue := futureValue / math.Pow(1+inflationRate/100, years)
+//func calculateValue(investment, expectedReturnRate, years float64) (float64, float64) {
+//	futureValue := investment * math.Pow(1+expectedReturnRate/100, years)
+//	futureActualReturnValue := futureValue / math.Pow(1+inflationRate/100, years)
+//	return futureValue, futureActualReturnValue
+//}
+
+// Alternative easier approach to returns
+func calculateValueDifferentReturn(investment, expectedReturnRate, years float64) (futureValue float64, futureActualReturnValue float64) {
+	futureValue = investment * math.Pow(1+expectedReturnRate/100, years)
+	futureActualReturnValue = futureValue / math.Pow(1+inflationRate/100, years)
 	return futureValue, futureActualReturnValue
+}
+
+func printNumber(num float64, decimalPlace int, prefix, formattingChar string) string {
+	formatter := "%." + strconv.Itoa(decimalPlace) + formattingChar
+	return fmt.Sprintf(prefix+formatter, num)
 }
